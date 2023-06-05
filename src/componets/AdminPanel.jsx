d3bb80b29e25bf3c6a25fbf0e31a109130d9632f
Nikita Smagin <n.smagin@agimagroup.ru>
2023-06-05 18:17:19 +0500

    Фиксы для админки

--- src/componets/AdminPanel.jsx
+++ src/componets/AdminPanel.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from "axios";
 
 const AdminPanel = () => {
@@ -9,6 +9,7 @@ const AdminPanel = () => {
     const [url, setUrl] = useState('')
     const [imageUrl, setImageUrl] = useState('')
     const [error, setError] = useState('')
+    const [list, setList] = useState([])
     const handleSubmit = async (e) => {
         e.preventDefault();
         const form = {
@@ -28,6 +29,12 @@ const AdminPanel = () => {
         setDescription('')
     }
 
+    const getList = async () => {
+        axios.get('https://wishlistbacknest.onrender.com/gifts')
+            .then(({data}) => setList(data))
+            .catch(e => console.error(e))
+    }
+
     const handleCancelReserve = async (e) => {
         e.preventDefault()
         if (!itemId) {
@@ -35,7 +42,7 @@ const AdminPanel = () => {
             return
         }
         try {
-           await axios.put(`https://wishlistbacknest.onrender.com/gifts/${itemId}`,{ isReserved: false} )
+            await axios.put(`https://wishlistbacknest.onrender.com/gifts/${itemId}`, {isReserved: false})
             setItemId('')
         } catch (e) {
             console.error(e)
@@ -47,22 +54,25 @@ const AdminPanel = () => {
     const addItem = async (data) => {
         try {
             await axios.post('https://wishlistbacknest.onrender.com/gifts', data)
-        }
-        catch (e) {
+        } catch (e) {
             console.error(e)
             setError(e?.response.data.message)
         }
     }
 
     const deleteGift = async () => {
-      try {
-        await axios.delete(`https://wishlistbacknest.onrender.com/gifts/${itemId}`)
-          setItemId('')
-      } catch (e) {
-          setError(e?.response.data.message)
-          console.error(e)
-      }
+        try {
+            await axios.delete(`https://wishlistbacknest.onrender.com/gifts/${itemId}`)
+            setItemId('')
+        } catch (e) {
+            setError(e?.response.data.message)
+            console.error(e)
+        }
     }
+
+    useEffect(() => {
+        getList()
+    })
     return (
         <div className='p-10'>
             <h2 className='text-2xl mb-5'>Добавить новый подарок</h2>
@@ -109,14 +119,25 @@ const AdminPanel = () => {
                         />
                     </div>
                 </div>
-                <button className='py-2 px-10 mx-auto border-2 border-black'>Submit</button>
+                <button className='py-2 px-10 mx-auto border-2 border-green-700 text-green-700 hover:text-white hover:bg-green-700'>Добавить подарок</button>
             </form>
-            <h2>Убрать из брони</h2>
+            <h2 className='mb-4'>Убрать из брони</h2>
             <form onSubmit={e => handleCancelReserve(e)}>
-                <input placeholder='ID' type="text" value={itemId} onChange={e => setItemId(e.target.value)}/>
-                <button className='py-2 px-10 mx-auto border-2 border-black ml-4'>Убрать из брони</button>
+                <div className='flex flex-col gap-2 mb-4'>
+                    <label htmlFor="city-select">Выберите подарок для удаления / отмены брони</label>
+                    <select className='max-w-[300px] h-[40px] p-2' name="city" id="city-select" onChange={(e) => setItemId(e.target.value)}>
+                        <option value="">-- Выберите подарок --</option>
+                        {!!list.length
+                            ? list.map(el => <option key={el.id} value={el.id}>{el.name}</option>)
+                            : <option value="null" disabled>Нет подарокв</option>
+                        }
+                    </select>
+                </div>
+                <div className='flex flex-col gap-2 max-w-[400px]'>
+                    <button className='py-2 px-10 border-2 border-black'>Убрать из брони</button>
+                    <button type="button" onClick={deleteGift} className='py-2 px-10 border-2 border-red-500 text-red-500'>Удалить</button>
+                </div>
             </form>
-            <button onClick={deleteGift} className='py-2 px-10 mx-auto border-2 border-black ml-4'>Удалить</button>
             <span className='text-2xl text-red-600'>{error}</span>
         </div>
     );
